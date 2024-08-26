8979a0091a8e587eefda7b3d6d2fa673a24355e4
neetharmaliackal <neethamaliackal@gmail.com>
2024-08-26 12:51:24 +0530

    Basic registartionForm without styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
+import {ReactiveFormsModule,FormsModule} from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { RegistrationFormComponent } from './registration-form/registration-form.component';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistrationFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     provideClientHydration()
---
+++ src/app/registration-form/registration-form.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import {FormControl,FormGroup} from "@angular/forms"
+import { HttpClient } from '@angular/common/http';
+import { Validators } from '@angular/forms';
+@Component({
+  selector: 'app-registration-form',
+  templateUrl: './registration-form.component.html',
+  styleUrl: './registration-form.component.css'
+})
+export class RegistrationFormComponent {
+
+userArray:any=[];
+userForm:FormGroup=new FormGroup({
+  id:new FormControl(''),
+  name:new FormControl('',[Validators.required,Validators.minLength(10)]),
+  userName:new FormControl(''),
+  email:new FormControl('')
+})
+constructor(private http: HttpClient){
+this.getAllUser();
+}
+
+getAllUser(){
+  this.http.get('https://jsonplaceholder.typicode.com/users').subscribe((res:any)=>
+  {
+  this.userArray=res;
+  })
+}
+onEdit(id:number){
+  this.http.get('https://jsonplaceholder.typicode.com/users'+ id).subscribe((res:any)=>
+  {
+    this.userForm=new FormGroup({
+      id:new FormControl(res.id),
+      name:new FormControl(res.name),
+      userName:new FormControl(res.username),
+      email:new FormControl(res.email)
+    })
+  })
+
+}
+
+OnSaveUser(){
+  debugger;
+const obj=this.userForm.value;
+this.http.post('https://jsonplaceholder.typicode.com/users',obj).subscribe((res:any)=>
+  {
+alert('user created');
+console.log(obj);
+  })
+}
+
+}
